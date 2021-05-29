82d897b7e3a9788353040ca5af2d4757a25f1c9b
xavi-barzallo <68974777+xavi-barzallo@users.noreply.github.com>
2021-05-29 16:20:37 -0500

    Creando Autores e Interfaz de Agregar Libro

---
+++ public/controladores/addLibro.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html> 
+<html> 
+    <head> 
+        <meta charset="UTF-8"> 
+        <title>Crear Nuevo Usuario</title> 
+        <style type="text/css" rel="stylesheet"> 
+        .error{ 
+        color: red; 
+        } 
+        </style> 
+    </head> 
+    <body> 
+        <?php 
+            //incluir conexión a la base de datos 
+            include '../../config/conexionBD.php'; 
+            $nombres = isset($_POST["nombres"]) ? trim($_POST["nombres"]) : null; 
+            $isbn = isset($_POST["isbn"]) ? trim($_POST["isbn"]) : null; 
+            $numero = isset($_POST["numero"]) ? trim($_POST["numero"])  : null; 
+           
+            $sql = "INSERT INTO libro VALUES (null, '$nombres', '$isbn', $numero)"; 
+          // echo ($sql);
+           
+            if ($conn->query($sql) === TRUE) { 
+                echo "<p>Se ha creado El Libro correctamemte!!</p>"; 
+            } else { 
+                if($conn->errno == 1062){ 
+                    echo "<p class='error'>El Libro con el $isbn ya esta registrada en el sistema </p>"; 
+                }else{ 
+                    echo "<p class='error'>Error: " . mysqli_error($conn) . "</p>"; 
+                } 
+            } 
+            
+                //cerrar la base de datos 
+            $conn->close(); 
+            echo "<a href='../vista/crear_usuario.html'>Regresar</a>"; 
+        ?> 
+    </body> 
+</html>
---
+++ public/vista/addLibro.php
@@ -0,0 +1,41 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Ingresar Libro</title>
+    <script type="text/javascript" src="validaciones_usuario.js"></script>
+    <link href="../controladores/estiloCrear.css" rel="stylesheet" />
+    <style type="text/css">
+        .error {
+            color: rgb(255, 230, 0);
+            font-size: 16px;
+        }
+    </style>
+</head>
+<div class="login">
+    <div class="login-header">
+        <h1>Ingreso del Libro</h1>
+    </div>
+    <div class="login-form">
+
+        <body>
+            <form id="formulario01" method="POST" action="../controladores/addLibro.php" >
+                <h3 for="nombres">Nombre (*)</h3>
+                <input type="text" id="nombres" name="nombres" value="" placeholder="Ingrese el nombre del libro ..."/>
+                <br>
+                <h3 for="nombres">ISBN (*)</h3>
+                <input type="text" id="isbn" name="isbn" value="" placeholder="Ingrese el ISBN ..."  />
+                
+                <br>
+                <h3 for="numero">Numero de Paginas (*)</h3>
+                <input type="text" id="numero" name="numero" value="" placeholder="Ingrese el Numero de Paginas ..."  />
+               
+                <br>
+                
+                <input type="submit" id="crear" name="crear" value="Aceptar" />
+                <input type="reset" id="cancelar" name="cancelar" value="Cancelar" onclick="location.href='login.html'" />
+            </form>
+    </div>
+</div>
+</body>
+
+</html>
